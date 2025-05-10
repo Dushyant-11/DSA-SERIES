1ded38f3f9daf6736de4e9ec78078051feddff39
Dushyant-11 <dkoded001@gmail.com>
2025-05-10 13:21:53 +0530

    added a new line

---
+++ Assignment 1/ques8&9.cpp
@@ -0,0 +1,17 @@
+// Finding output //
+#include <iostream>
+using namespace std;
+
+int main () {
+    /* int a=4;
+    int b=5;
+    a++, b--;
+    cout << a++<<" "<<b--; */
+
+
+int x=5;
+cout << x++ << " " << ++x << endl;
+
+    return 0;
+
+}
