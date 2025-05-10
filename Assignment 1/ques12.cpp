a8c9912dd190d2dcb5ca1b28ee41283b506477d0
Dushyant-11 <dkoded001@gmail.com>
2025-05-10 13:48:03 +0530

    added a new line

---
+++ Assignment 1/ques12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x=4, y=2, z;
+z = x++ * --y + ++x;
+    cout << x << " " << y << " " << z << endl;
+    return 0;
+}
+
+// output //
+/* 6 1 10 */
