013f4e3420a4e1b8c04b50a1b6e368f51b330c3c
Dushyant-11 <dkoded001@gmail.com>
2025-05-10 13:54:38 +0530

    added a new line

--- Assignment 1/ques12.cpp
+++ Assignment 1/ques12.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main () {
     int x=4, y=2, z;
-z = x++ * --y + ++x;
-    cout << x << " " << y << " " << z << endl;
+//z = x++ * --y + ++x;//
+   // cout << x << " " << y << " " << z << endl;//
+   cout << x << y << endl;
+
     return 0;
 }
 
